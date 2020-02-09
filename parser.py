1114611cf96aa96b7463bad81b548e0cae5026b0
Kevini05 <kevini05_01@hotmail.com>
2020-02-09 04:28:48 +0100

    Revisar PLR en iperf UDP, funcional tiempos de transmision

--- parser.py
+++ parser.py
@@ -8,6 +8,8 @@ import time
 
 errors = []
 bit_rates = []
+missing_packets = []
+transmission_time = []
 
 
 def simulation(shell_file, opt):
@@ -66,7 +68,7 @@ def parse_iperf(output_file, opt):
         file_content = [[x[i] for i in range(6, len(x)) if x[i] not in ["GBytes", "MBytes", "KBytes", "Bytes"]] for x in
                         file_content]
         file_content = [[y for y in x] for x in file_content if len(x) > 0]
-        file_content = file_content[:opt.simulation_time]
+        file_content = file_content[:len(file_content)]
         # print(file_content, " ", len(file_content))
         return file_content
 
@@ -106,7 +108,8 @@ def parser_bit_rate_iperf(file_content, opt):
         elif x[1] == "Gbits/sec" and float(x[0]) < 1:
             tmp += float(x[0]) * 1000
             bit_rate.append(float(x[0]) * 1000)
-
+    transmission_time.append(len(bit_rate))
+    print("Transmission time: ",len(bit_rate))
     bit_rates.append(tmp / len(bit_rate))
     print("Bit Rate Average: ", tmp / len(bit_rate))
     # plotting(bit_rate, opt.simulation_time, labels, title)
@@ -129,7 +132,7 @@ def parse_udptetrys(output_file, opt):
         file_content = [
             [ansi_escape.sub('', x[i]) for i in range(0, len(x))] for x in file_content if len(x) == n]
 
-        print(file_content, " ", len(file_content))
+        # print(file_content, " ", len(file_content))
         return file_content
 
 
@@ -144,8 +147,11 @@ def parser_bit_rate_udptetrys(file_content, opt):
             tmp += float(x[1])
             bit_rate.append(float(x[1]))
 
+    transmission_time.append(len(bit_rate))
+    print("Transmission time: ",len(bit_rate))
+    bit_rates.append(tmp / len(bit_rate))
     print("Bit Rate Average: ", tmp / len(bit_rate))
-    plotting(bit_rate, len(bit_rate), labels, title)
+    # plotting(bit_rate, len(bit_rate), labels, title)
 
 
 def parser_error_udptetrys(file_content, opt):
@@ -155,11 +161,12 @@ def parser_error_udptetrys(file_content, opt):
     title = 'Error Rate vs Time at ' + opt.role + ' side '
 
     for x in file_content:
-        tmp += float(x[4].split('%')[0])
-        error.append(float(x[4].split('%')[0]))
+        tmp += float(x[3])
+    
 
-    print("Error Average: ", tmp / len(error))
-    plotting(error, len(error), labels, title)
+    errors.append(float(file_content[-1][6])/tmp*100)
+    print("Error Average: ", float(file_content[-1][6])/tmp*100)
+    # plotting(error, len(error), labels, title)
 
 
 def parser_missing_udptetrys(file_content, opt):
@@ -177,8 +184,9 @@ def parser_missing_udptetrys(file_content, opt):
         tmp += float(x[n])
         missing.append(float(x[n]))
 
+    missing_packets.append(tmp / len(missing))
     print("Missing Average: ", tmp / len(missing))
-    plotting_stem(missing, len(missing), labels, title)
+    # plotting_stem(missing, len(missing), labels, title)
 
 
 def plotting(y, x, labels, title):
@@ -222,26 +230,48 @@ def client_interface(ip_address):
 def chose_role(opt):
     if opt.role == "client" and opt.tool == "iperf":
         ## UDP
-        # python3 ./parser.py --role client --tool iperf --protocol udp --delay 100 --loss 3 --ip 10.0.0.1 --rate 2 --time 60
+        # python3 ./parser.py --role client --tool iperf --protocol udp --delay 100 --loss 3 --ip 10.0.0.1 --rate 2 --bytes 100000
         ## TCP
-        # python3 ./parser.py --role client --tool iperf --delay 100 --loss 3 --ip 10.0.0.1 --rate 2 --time 60
+        # python3 ./parser.py --role client --tool iperf --delay 100 --loss 3 --ip 10.0.0.1 --rate 2 --bytes 100000
         m = client_interface(opt.ip_address)
 
-        shell_file = "./" + opt.tool + "_" + opt.role + ".sh -d " + str(opt.delay_channel) + " -l " + str(
-            opt.packet_loss_rate) + " -a " + opt.ip_address + " -b " + str(opt.bit_rate) + " -t " + str(
-            opt.simulation_time) + " -m " + m + " -p " + opt.protocol
+        os.system(f"tc qdisc add dev {m} root netem delay {opt.delay_channel}ms loss {opt.packet_loss_rate}%")
+        if opt.protocol == "udp":
+            command =f"iperf -c {opt.ip_address} -u -i 1 -b {opt.bit_rate}Mb -n {opt.number_bytes}"
+        else:
+            command =f"iperf -c {opt.ip_address} -i 1 -b {opt.bit_rate}Mb -n {opt.number_bytes}"
 
-        simulation(shell_file, opt)
+        print("Command Client: ",command)
+        print("--------------------------------------------")
+        os.system(f"{command} | tee ./tmp_client.txt")
+        time.sleep(3.0)
         os.system("killall -9 iperf")
+        os.system(f"tc qdisc delete dev {m} root netem")
+
+        file_content = parse_iperf("./tmp_client.txt", opt)
+        parser_bit_rate_iperf(file_content, opt)
+
 
     elif opt.role == "server" and opt.tool == "iperf":
         ## UDP
-        # python3 ./parser.py --role server --tool iperf  --protocol udp --time 60
+        # python3 ./parser.py --role server --tool iperf  --protocol udp
         ## TCP
-        # python3 ./parser.py --role server --tool iperf  --time 60
-        # shell_file = "./" + opt.tool + "_" + opt.role + ".sh -t" + str(opt.simulation_time +1) + " -p " + opt.protocol
-        shell_file = "./" + opt.tool + "_" + opt.role + ".sh -p " + opt.protocol
-        simulation(shell_file, opt)
+        # python3 ./parser.py --role server --tool iperf
+        os.system("killall -9 iperf")
+        if opt.protocol == "udp":
+            command =f"iperf -s -u -i 1"
+            
+        else:
+            command =f"iperf -s -i 1"
+
+        print("Command Server: ",command)
+        print("--------------------------------------------")
+        os.system(f"{command} | tee ./tmp_server.txt")
+        file_content = parse_iperf("./tmp_server.txt", opt)
+        parser_bit_rate_iperf(file_content, opt)
+        if opt.protocol == "udp":
+            parser_error_iperf(file_content, opt)
+        
 
     elif opt.tool == "ping":
         # python3 ./parser.py --tool ping --delay 100 --loss 3 --ip 10.0.0.1 --rate 2 --count 1000
@@ -254,24 +284,36 @@ def chose_role(opt):
 
     elif opt.role == "client" and opt.tool == "udptetrys":
         # python3 ./parser.py --role client --tool udptetrys --delay 100 --loss 3 --ip 10.0.0.2 --rate 2 --time 60
+        print("udptetrys client")
+        opt.protocol = "udp"
         m = client_interface(opt.ip_address)
-        delay_tetrys = 1400 * 8 / (opt.bit_rate * 1000)
-        number_of_packets = opt.bit_rate * 1e6 * opt.simulation_time / 11200  # 8 bits/byte * 1400 bytes/packet
+        number_packets = int(opt.number_bytes/1408)
+        delay_udp = (1400*8)/(opt.bit_rate*1000) # in miliseconds
+
+
         os.system(f"tc qdisc add dev {m} root netem delay {opt.delay_channel}ms loss {opt.packet_loss_rate}%")
-        os.system(
-            f"./udptetrys -c {opt.ip_address} -d {delay_tetrys} -b 1 -z 10 -k 5 -n {number_of_packets} | tee ./udptetrys_client.txt")
+        command =f"./udptetrys -c {opt.ip_address} -d {delay_udp} -b 1 -z 10 -k 5 -n {number_packets}"
+        print("Command Client: ",command)
+        print("--------------------------------------------")
+        os.system(f"{command} | tee ./tmp_client.txt")
+        time.sleep(3.0)
         os.system("killall -9 udptetrys")
         os.system(f"tc qdisc delete dev {m} root netem")
-        print("Client.txt done!!!!")
-        file_content = parse_udptetrys("udptetrys_client.txt", opt)
+
+        file_content = parse_udptetrys("tmp_client.txt", opt)
         parser_bit_rate_udptetrys(file_content, opt)
         parser_missing_udptetrys(file_content, opt)
 
     elif opt.role == "server" and opt.tool == "udptetrys":
         # python3 ./parser.py --role server --tool udptetrys
         print("udptetrys server")
-        os.system(f"./udptetrys -s | tee ./udptetrys_server.txt")
-        file_content = parse_udptetrys("udptetrys_server.txt", opt)
+        opt.protocol = "udp"
+        os.system("killall -9 udptetrys")
+        a = "./udptetrys -s"
+        print("Command Server: ",a)
+        print("--------------------------------------------")
+        os.system("./udptetrys -s | tee ./tmp_server.txt")
+        file_content = parse_udptetrys("tmp_server.txt", opt)
         parser_bit_rate_udptetrys(file_content, opt)
         parser_missing_udptetrys(file_content, opt)
         parser_error_udptetrys(file_content, opt)
@@ -290,51 +332,92 @@ ap.add_argument("--loss", action="store", dest="packet_loss_rate", type=int, def
                 help="Packet loss rate in percentage.")
 ap.add_argument("--ip", action="store", dest="ip_address", default="", help="IP address")
 ap.add_argument("--rate", action="store", dest="bit_rate", type=int, default=2, help="Bit rate in Mbps")
-ap.add_argument("--time", action="store", dest="simulation_time", type=int, default=70,
-                help="Simulation time in seconds")
+ap.add_argument("--bytes", action="store", dest="number_bytes", type=int, default=5000000,
+                help="Number of bytes sent by the client")
 ap.add_argument("--count", action="store", dest="count_icmp_packets", type=int, default=100,
                 help="Count of icmp packets to send")
 opt = ap.parse_args()
 
 # ------------------------------------------------------
-for loss in [1, 2, 5]:
-    for debit in [1, 5, 10]:
-        for delay in [100, 200, 500]:
+loss_range = [1]#[1, 2, 5]          # en pourcentage
+debit_range = [1, 5, 10]        # en Mbps
+delay_range = [100, 200, 500]   # en ms
+
+
+for loss in loss_range:
+    for debit in debit_range:
+        for delay in delay_range:
             opt.bit_rate = debit
             opt.packet_loss_rate = loss
             opt.delay_channel = delay
-
-            print (opt.protocol," Bit Rate", opt.bit_rate,"  Loss",opt.packet_loss_rate, "   Delay", opt.delay_channel)
+            print("#############################################")
+            print("Protocol: ",opt.protocol,"  Loss",opt.packet_loss_rate,"    Bit Rate", opt.bit_rate,"   Delay", opt.delay_channel)
             chose_role(opt)
 
-filename = opt.role+"_"+opt.tool+".txt"
-f = open(filename, 'w')
-f.write("hello\talex")
 
-if opt.role == "server" and opt.protocol == "udp":
-    print(errors)
+
+filename = opt.role+"_"+opt.tool+"_"+opt.protocol+".txt"
+f = open(filename, 'w')
+
+# print(bit_rates)
+f.write("########## Bit Rate ##########\n")
+i = 0
+for loss in loss_range:
+    f.write(f"---------- Loss: {loss}% -------------\n")
+    for debit in debit_range:
+        if debit == debit_range[0]:
+            f.write(f"Delay [ms]\t\t{delay_range[0]}\t{delay_range[1]}\t{delay_range[2]}\n")
+            f.write("Bit rate [Mbps]\n")
+        f.write(f"{debit}\t\t\t{bit_rates[i]:.3f}\t{bit_rates[i+1]:.3f}\t{bit_rates[i+2]:.3f}\n")
+        i+=3
+    f.write("\n")
+
+# print(transmission_time)
+f.write("########## Transmission Time ##########\n")
+i = 0
+for loss in loss_range:
+    f.write(f"---------- Loss: {loss}% -------------\n")
+    for debit in debit_range:
+        if debit == debit_range[0]:
+            f.write(f"Delay [ms]\t\t{delay_range[0]}\t{delay_range[1]}\t{delay_range[2]}\n")
+            f.write("Bit rate [Mbps]\n")
+        f.write(f"{debit}\t\t\t{transmission_time[i]:.3f}\t{transmission_time[i+1]:.3f}\t{transmission_time[i+2]:.3f}\n")
+        i+=3
+    f.write("\n")
+
+
+if opt.role == "server" and (opt.protocol == "udp" or opt.tool == "udptetrys"):
+    # print(errors)
+    f.write("########## Packet Loss Rate ##########\n")
     i = 0
-    for loss in [1, 2, 5]:
-        print(f"Loss: {loss}")
-        for debit in [1, 5, 10]:
-            if debit == 1:
-                print("Delay [ms]\t\t100\t200\t500")
-                print("Bit rate [Mbps]")
-            print(f"{debit}\t\t\t{errors[i]:.3f}\t{errors[i+1]:.3f}\t{errors[i+2]:.3f}")
+    for loss in loss_range:
+        f.write(f"---------- Loss: {loss}% -------------\n")
+        for debit in debit_range:
+            if debit == debit_range[0]:
+                f.write(f"Delay [ms]\t\t{delay_range[0]}\t{delay_range[1]}\t{delay_range[2]}\n")
+                f.write("Bit rate [Mbps]\n")
+            f.write(f"{debit}\t\t\t{errors[i]:.3f}\t{errors[i+1]:.3f}\t{errors[i+2]:.3f}\n")
             i+=3
-        print()
+        f.write("\n")
         
 
-print(bit_rates)
-
 i = 0
-for loss in [1, 2, 5]:
-    print(f"Loss: {loss}")
-    for debit in [1, 5, 10]:
-        if debit == 1:
-            print("Delay [ms]\t\t100\t200\t500")
-            print("Bit rate [Mbps]")
-        print(f"{debit}\t\t\t{bit_rates[i]:.3f}\t{bit_rates[i+1]:.3f}\t{bit_rates[i+2]:.3f}")
-        i+=3
-    print()
+
+if opt.tool == "udptetrys":
+    # print(missing_packets)
+    f.write("########## Missing Packets ##########\n")
+    i = 0
+    for loss in loss_range:
+        f.write(f"---------- Loss: {loss}% -------------\n")
+        for debit in debit_range:
+            if debit == debit_range[0]:
+                f.write(f"Delay [ms]\t\t{delay_range[0]}\t{delay_range[1]}\t{delay_range[2]}\n")
+                f.write("Bit rate [Mbps]\n")
+            f.write(f"{debit}\t\t\t{missing_packets[i]:.3f}\t{missing_packets[i+1]:.3f}\t{missing_packets[i+2]:.3f}\n")
+            i+=3
+        f.write("\n")
+
+
+
+
 
